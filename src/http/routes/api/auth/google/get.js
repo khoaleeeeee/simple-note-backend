f9a09ae081434644ab01588272cf6a1a5cf5265b
Khoa Le <kle@screenbeam.com>
2024-05-08 14:12:51 -0700

    use sameSite none for cookie when sending

--- src/http/routes/api/auth/google/get.js
+++ src/http/routes/api/auth/google/get.js
@@ -36,7 +36,7 @@ const get = async (req, res) => {
     res.cookie("sessionToken", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? 'None' : 'Lax',
+      sameSite: 'None',
       maxAge: 1000 * 60 * 60, // 1 hour
     });
 
